9238cea35654169d31366885b11a4d3a0153e8b3
lingwangshu018 <lingwangshu018@gmail.com>
2026-07-25 00:42:02 +0800

    fix: load room artwork styles in GitHub Pages entry

--- github-pages/main.tsx
+++ github-pages/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../app/globals.css";
+import "../app/room-backgrounds.css";
 import Home from "../app/page";
 
 const root = document.getElementById("root");
